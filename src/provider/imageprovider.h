4fd41ff9d8e310b5559ca91b56712db4048777b7
Enrico <enrico@kkss-mbp.ukd69.med.tu-dresden.de>
2015-02-12 11:08:08 +0100

    Bilder aus HDF5

---
+++ src/provider/imageprovider.h
@@ -0,0 +1,39 @@
+#ifndef IMAGEPROVIDER_H
+#define IMAGEPROVIDER_H
+
+#include <QObject>
+#include <QString>
+#include <QSize>
+#include <QImage>
+#include <QQuickImageProvider>
+
+#include "src/import/importhdf5.h"
+#include "src/provider/MyClass.h"
+
+class ImageProvider : public QQuickImageProvider
+{
+public:
+    explicit ImageProvider();
+    ~ImageProvider();
+
+    void setMouseArea(QObject *area);
+    void readData();
+    QImage requestImage(const QString &id, QSize *size, const QSize &requestedSize);
+
+private:
+    int imageNumber;
+
+    QObject *mouseArea;
+    QString localFile;
+    QImage *oldImage;
+    QImage newImage;
+
+    QList<int> listOfColors;
+    QList<QList<int>> listOfImageColors;
+
+    QList<QPoint> listOfPoints;
+    QList<QList<QPoint>> listOfPolygons;
+    QList<QList<QList<QPoint>>> listOfImages;
+};
+
+#endif // IMAGEPROVIDER_H
