cd6d5acd34491d42ff85543e0ce27a8c3885c413
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-01-11 12:26:41 +0100

    moved function documentation from header to source file

--- src/io/hdf5_aux.h
+++ src/io/hdf5_aux.h
@@ -11,113 +11,25 @@
 
 /* extension of H5 namespace to allow enabling/disabling of error-printing */
 namespace H5 {
-/*!
- * \brief disables all HDF5 error-printing
- */
 void disableErrors();
-/*!
- * \brief enables HDF5 error-printing again
- */
 void enableErrors();
 }
 
 /* information about objects in HDF5 */
-/*!
- * \brief Tests if a certain Group with name "name" exists in the
- * given Group or File "cfg"
- *
- * \param cfg The Group or File to search in
- * \param name The name of the Group to search
- * \return true if it exists, false if it doesn't
- */
 bool groupExists(H5::CommonFG &cfg, const char *name);
-/*!
- * \brief Tests if a certain DataSet with name "name" exists in the
- * given Group or File "cfg"
- *
- * \param cfg The Group or File to search in
- * \param name The name of the DataSet to search
- * \return true if it exists, false if it doesn't
- */
 bool datasetExists(H5::CommonFG &cfg, const char *name);
-/*!
- * \brief Tests if a certain Link (i.e. soft or hard link to Group,
- * Dataset, etc.) with name "name" exists in the given Group or File "cfg"
- *
- * \param cfg The Group or File to search in
- * \param name The name of the Link to search
- * \return true if it exists, false if it doesn't
- */
 bool linkExists(H5::CommonFG &cfg, const char *name);
-/*!
- * \brief For a given group of name "name" in the Group with GroupID
- * "gid" returns the number of Elements in this Group
- *
- * \param gid HDF5-GroupID that holds the group to inspect
- * \param name name of the group
- * \return number of elements in that group
- */
 hsize_t getGroupSize(hid_t gid, const char *name);
 
-/*!
- * \brief HDF5-callback that is used by collectGroupElementNames. Adds to the
- * given op_data (which has to be of type std::list<std::string>*) the name of
- * the current element
- *
- * \param group_id the HDF5-GroupID that holds the group to inspect
- * \param name the name of the group to inspect
- * \param op_data a std::list<std::string>* in which the names of the elements
- * are collected
- */
 herr_t add_group_element_name(hid_t group_id, const char *name, void *op_data);
-/*!
- * \brief collects all the names of the elements of a group
- * \param cfg Group or File for which the names should be collected
- * \return a std::list of std::string with the names
- */
 std::list<std::string> collectGroupElementNames(H5::CommonFG &cfg);
 
-/*!
- * \brief returns the type of the link
- * \param obj the object for which the link type should be returned
- * \return the link type
- */
 H5L_type_t getLinkType(H5::H5Object &obj);
 
 /* convenience functions */
-/*!
- * \brief opens or creates a DataSet
- * \param cfg where to open or create the DataSet
- * \param name which name to use
- * \param type the DataType to use if it doesn't exist
- * \param space the DataSpace to use if it doesn't exist
- * \return the opened or created DataSet
- */
 H5::DataSet openOrCreateDataSet(H5::CommonFG& cfg, const char *name, H5::DataType type, H5::DataSpace space);
-/*!
- * \brief opens or creates a Group
- * \param cfg where to open or create the Group
- * \param name which name to use
- * \param size (optional) size hint
- * \return the opened or created Group
- */
 H5::Group openOrCreateGroup(H5::CommonFG& cfg, const char *name, int size = 0);
-/*!
- * \brief clears or creates a Group
- * \param cfg where to clear or create the Group
- * \param name which name to use
- * \param size (optional) size hint
- * \return the cleared or newly created Group
- */
 H5::Group clearOrCreateGroup(H5::CommonFG& cfg, const char *name, int size = 0);
-/*!
- * \brief links to a given target. If the link already exists, it will be
- * overwritten
- * \param type type of the link to create
- * \param grp where to create the link
- * \param target the target of the link
- * \param link_name the name of the link
- */
 void linkOrOverwriteLink(H5L_type_t type, H5::Group grp, std::string target, std::string link_name);
 
 /* read/write templates */
