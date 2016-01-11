afd562872c1c14a1acfaf6180b1ad2d7f504626b
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-01-11 14:54:19 +0100

    documentation for slice.*, deprecate some functions

--- src/base/slice.cpp
+++ src/base/slice.cpp
@@ -4,45 +4,102 @@
 
 namespace CellTracker {
 
-Slice::Slice() : sliceId(0), slicePos(std::shared_ptr<QPoint>(new QPoint(0,0))) { }
+/*!
+ * \brief constructs a Slice
+ *
+ * \warning Do not use this.
+ */
+Slice::Slice() : sliceId(UINT32_MAX), frameId(UINT32_MAX), slicePos(std::shared_ptr<QPoint>(new QPoint(0,0))) { }
 
+/*!
+ * \brief constructs a Slice
+ *
+ * \param id the SliceID of the Slice
+ * \param frameId the FrameID of the Slice
+ */
 Slice::Slice(uint32_t id, uint32_t frameId) : sliceId(id), frameId(frameId), slicePos(std::shared_ptr<QPoint>(new QPoint(0,0))) {}
 
+/*!
+ * \brief destructs a Slice
+ */
 Slice::~Slice() { }
 
+/*!
+ * \brief adds a Channel to a Slice
+ * \param channel the Channel to add to this Slice
+ */
 void Slice::addChannel(std::shared_ptr<Channel> channel)
 {
     channels.insert(channel->getChanId(),channel);
 }
 
+/*!
+ * \brief returns a Channel in this Slice
+ * \param id the ID of the Channel
+ * \return the Channel
+ */
 std::shared_ptr<Channel> Slice::getChannel(uint32_t id) const
 {
     return channels.value(id,nullptr);
 }
 
+/*!
+ * \brief returns the Position of the Slice
+ * \return the Position of this Slice
+ * \warning Do not use this.
+ */
 std::shared_ptr<QPoint> Slice::getSlicePos() const
 {
     return slicePos;
 }
 
+/*!
+ * \brief sets the Position of the Slice
+ * \param value the Position of this Slice
+ * \warning Do not use this.
+ */
 void Slice::setSlicePos(const std::shared_ptr<QPoint> &value)
 {
     slicePos = value;
 }
 
+/*!
+ * \brief sets the Position of the Slice using x- and y-coordinates
+ * \param x the x-component of the Position of this Slice
+ * \param y the y-component of the Position of this Slice
+ * \warning Do not use this.
+ */
 void Slice::setSlicePos(int x, int y)
 {
     slicePos = std::shared_ptr<QPoint>(new QPoint(x,y));
 }
 
+/*!
+ * \brief returns all Channels of a Slice
+ *
+ * Returns a QHash of the Channels of this Slice using the SliceID as key
+ * to the QHash
+ *
+ * \return the QHash containing the Channels
+ */
 QHash<uint32_t, std::shared_ptr<Channel> > Slice::getChannels() const
 {
     return channels;
 }
+
+/*!
+ * \brief returns the SliceID of this Channel
+ * \return the sliceID of this Channel
+ */
 uint32_t Slice::getSliceId() const
 {
     return sliceId;
 }
+
+/*!
+ * \brief returns the FrameID of this Channel
+ * \return the frameID of this Channel
+ */
 uint32_t Slice::getFrameId() const
 {
     return frameId;
