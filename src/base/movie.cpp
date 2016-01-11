8ab753b6452a22fbba52383f5fea8e0b5118f3b3
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-01-11 14:15:49 +0100

    documentation for movie.*

--- src/base/movie.cpp
+++ src/base/movie.cpp
@@ -2,20 +2,43 @@
 
 namespace CellTracker {
 
+/*!
+ * \brief constructs a Movie
+ */
 Movie::Movie() {}
 
+/*!
+ * \brief desctructs a Movie
+ */
 Movie::~Movie() {}
 
+/*!
+ * \brief adds a Frame to the Movie
+ * \param f the Frame to add
+ */
 void Movie::addFrame(std::shared_ptr<Frame> f)
 {
     frames.insert(f->getID(),f);
 }
 
+/*!
+ * \brief returns a Frame in a Movie
+ * \param id the ID of the Frame to return
+ * \return the Frame
+ */
 std::shared_ptr<Frame> Movie::getFrame(uint32_t id) const
 {
     return frames.value(id, nullptr);
 }
 
+/*!
+ * \brief returns a QHash of all Frames in the Movie
+ *
+ * This functions returns a QHash of all Frames in the Movie, where the FrameID
+ * of the Frames is used as a key to the QHash
+ *
+ * \return the QHash of all Frames in the Movie
+ */
 QHash<uint32_t,std::shared_ptr<Frame>> Movie::getFrames()
 {
     return frames;
