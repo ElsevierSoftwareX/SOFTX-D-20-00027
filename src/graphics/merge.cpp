385f3581055cc317d0d35ffce9d889676e7d882c
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-03-21 19:25:45 +0100

    begin merge functionallity

--- src/graphics/merge.cpp
+++ src/graphics/merge.cpp
@@ -1,6 +1,23 @@
 #include "merge.h"
 
-Merge::Merge()
-{
+#include <QDebug>
+#include <QLine>
+#include <QPainterPath>
 
+Merge::Merge() { }
+
+QPolygonF &Merge::compute(QPolygonF &first, QPolygonF &second)
+{
+    /* find the tangents from the center of the first polygon to the second polygon */
+    QPointF c1 = first.boundingRect().center();
+    for (QPointF &p : second) {
+        QLineF line(c1, p);
+        int intersectionCount = 0;
+        for (int i = 0; i < second.size(); i++) {
+            QLineF segment(second[i], second[(i+1)%second.size()]);
+            if (line.intersect(segment, nullptr) == QLineF::IntersectType::BoundedIntersection)
+                intersectionCount++;
+        }
+        qDebug() << line << "has" << intersectionCount << "intersections with second";
+    }
 }
--- src/graphics/merge.h
+++ src/graphics/merge.h
@@ -1,11 +1,13 @@
 #ifndef MERGE_H
 #define MERGE_H
 
+#include <QPolygonF>
 
 class Merge
 {
 public:
     Merge();
+    static QPolygonF &compute(QPolygonF&, QPolygonF&);
 };
 
 #endif // MERGE_H
