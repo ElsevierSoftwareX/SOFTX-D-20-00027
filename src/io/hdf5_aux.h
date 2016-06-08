65ac03642f9bef654b402f85d77e224204b74ddf
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-06-08 21:33:04 +0200

    no need to initialize value, as it is only written

--- src/io/hdf5_aux.h
+++ src/io/hdf5_aux.h
@@ -73,7 +73,7 @@ template <typename T> inline T readSingleValue(hid_t dset_id) {
  * \brief wrapper for readSingleValue(H5::DataSet)
  */
 template <typename T> inline T readSingleValue(H5::CommonFG &group, const char *name) {
-    T ret{};
+    T ret;
     H5::DataSet dset = group.openDataSet(name);
 
     ret = readSingleValue<T>(dset);
