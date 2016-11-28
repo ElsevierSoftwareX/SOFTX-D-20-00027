342ad7a436316c23a6163f16d9e5bca6afa5148e
Sebastian Wagner <s0471516@mail.zih.tu-dresden.de>
2016-11-28 08:15:40 +0100

    Remove comment, add todo

--- src/provider/timetracker.cpp
+++ src/provider/timetracker.cpp
@@ -1,4 +1,5 @@
 #include "timetracker.h"
+
 #include <chrono>
 #include <QThread>
 
@@ -13,16 +14,17 @@ void TimeTracker::run() {
     using namespace std::chrono;
     GUIState *gs = GUIState::getInstance();
 
-    while (true && !this->isInterruptionRequested()) {
+    while (!this->isInterruptionRequested()) {
         QString path = gs->getProjPath();
         if (path.compare("") != 0) {
             uint64_t last64 = gs->getWorkedOnProject();
-            //    uint64_t last64 = 0;
             steady_clock::duration last = seconds(last64);
             steady_clock::duration next = last + poll_interval;
 
             std::this_thread::sleep_for(poll_interval);
             GUIState::getInstance()->setWorkedOnProject(duration_cast<seconds>(next).count());
+
+            /*! \todo write to HDF-file */
         } else {
             this->msleep(100);
         }
